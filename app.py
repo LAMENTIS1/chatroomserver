25cb7e2735ac02f05bb37bf8a48b22d07910da75
LAMENTIS1 <124012543+LAMENTIS1@users.noreply.github.com>
2024-08-29 11:54:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit, join_room, leave_room
-from flask_cors import CORS  # Add this import
-from engineio.payload import Payload
 
+# Next two lines are for the issue: https://github.com/miguelgrinberg/python-engineio/issues/142
+from engineio.payload import Payload
 Payload.max_decode_packets = 200
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "thisismys3cr3tk3y"
 
-CORS(app)  # Add this line to handle CORS
-
 socketio = SocketIO(app)
 
-_users_in_room = {}
-_room_of_sid = {}
-_name_of_sid = {}
+
+_users_in_room = {} # stores room wise user list
+_room_of_sid = {} # stores room joined by an used
+_name_of_sid = {} # stores display name of users
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         room_id = request.form['room_id']
         return redirect(url_for("entry_checkpoint", room_id=room_id))
+
     return render_template("home.html")
 
 @app.route("/room/<string:room_id>/")
@@ -35,14 +36,18 @@ def entry_checkpoint(room_id):
         display_name = request.form['display_name']
         mute_audio = request.form['mute_audio']
         mute_video = request.form['mute_video']
-        session[room_id] = {"name": display_name, "mute_audio": mute_audio, "mute_video": mute_video}
+        session[room_id] = {"name": display_name, "mute_audio":mute_audio, "mute_video":mute_video}
         return redirect(url_for("enter_room", room_id=room_id))
+
     return render_template("chatroom_checkpoint.html", room_id=room_id)
+    
+
 
 @socketio.on("connect")
 def on_connect():
     sid = request.sid
-    print(f"New socket connected {sid}")
+    print("New socket connected ", sid)
+    
 
 @socketio.on("join-room")
 def on_join_room(data):
@@ -50,22 +55,26 @@ def on_join_room(data):
     room_id = data["room_id"]
     display_name = session[room_id]["name"]
     
+    # register sid to the room
     join_room(room_id)
     _room_of_sid[sid] = room_id
     _name_of_sid[sid] = display_name
     
-    print(f"[{room_id}] New member joined: {display_name}<{sid}>")
+    # broadcast to others in the room
+    print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
     emit("user-connect", {"sid": sid, "name": display_name}, broadcast=True, include_self=False, room=room_id)
     
+    # add to user list maintained on server
     if room_id not in _users_in_room:
         _users_in_room[room_id] = [sid]
-        emit("user-list", {"my_id": sid})
+        emit("user-list", {"my_id": sid}) # send own id only
     else:
-        usrlist = {u_id: _name_of_sid[u_id] for u_id in _users_in_room[room_id]}
-        emit("user-list", {"list": usrlist, "my_id": sid})
-        _users_in_room[room_id].append(sid)
+        usrlist = {u_id:_name_of_sid[u_id] for u_id in _users_in_room[room_id]}
+        emit("user-list", {"list": usrlist, "my_id": sid}) # send list of existing users to the new member
+        _users_in_room[room_id].append(sid) # add new member to user list maintained on server
 
     print("\nusers: ", _users_in_room, "\n")
+
 
 @socketio.on("disconnect")
 def on_disconnect():
@@ -73,7 +82,7 @@ def on_disconnect():
     room_id = _room_of_sid[sid]
     display_name = _name_of_sid[sid]
 
-    print(f"[{room_id}] Member left: {display_name}<{sid}>")
+    print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
     emit("user-disconnect", {"sid": sid}, broadcast=True, include_self=False, room=room_id)
 
     _users_in_room[room_id].remove(sid)
@@ -85,15 +94,16 @@ def on_disconnect():
 
     print("\nusers: ", _users_in_room, "\n")
 
+
 @socketio.on("data")
 def on_data(data):
     sender_sid = data['sender_id']
     target_sid = data['target_id']
     if sender_sid != request.sid:
-        print(f'[Not supposed to happen!] request.sid and sender_id don\'t match!!!')
+        print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
 
     if data["type"] != "new-ice-candidate":
-        print(f'{data["type"]} message from {sender_sid} to {target_sid}')
+        print('{} message from {} to {}'.format(data["type"], sender_sid, target_sid))
     socketio.emit('data', data, room=target_sid)
 
 if __name__ == "__main__":
